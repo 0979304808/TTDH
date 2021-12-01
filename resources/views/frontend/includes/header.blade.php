8cf642595c35eb548e42a00be9bef5fd260ea3f8
0979304808 <hungh5614@gmail.com>
2021-12-01 20:08:10 +0700

    update

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -35,17 +35,15 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
 
-
                     <li class="dropdown {{ Request::is('tin-tuc') ? 'active' : '' }}">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tin Tức </a>
-                        <ul class="list-news">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tin tức <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
                             <li class="item-news"><a class="title-news" href="#">Tư Liệu</a></li>
                             <li class="item-news"> <a class="title-news" href="#">Phân tích</a></li>
                             <li class="item-news"><a class="title-news" href="#">Người Việt 5 châu</a></li>
                             <li class="item-news"><a class="title-news" href="#">Cuộc sống đó đây</a></li>
                         </ul>
                     </li>
-
 
                     <li class="dropdown {{ Request::is('cong-dong') ? 'active' : '' }}"><a href="/cong-dong"> Cộng đồng<span class="sr-only">(current)</span></a>
 {{--                        <ul class="list-news">--}}
@@ -70,12 +68,14 @@
                     </li>
                     @if(Auth::check())
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
-                            <ul class="list-news">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
                                 <li class="item-news"><a class="title-news" href="#">Thông tin tài khoản</a></li>
                                 <li class="item-news"> <a class="title-news" href="{{ route('frontend.logout') }}">Logout</a></li>
                             </ul>
                         </li>
+
+
                         @else
                         <li class="dropdown"><a href="#" onclick="showFunction()" id="sig-in" data-toggle="modal" data-target="#myModal ">Đăng nhập</a>
                         </li>
@@ -87,57 +87,5 @@
     </div>
 </nav>
 
-<style>
-    .list-news {
-        position: absolute;
-        list-style: none;
-        display: none;
-        background-color: #fff;
-        width: 165px;
-        padding: 6px 6px 10px 6px;
-        border-top: solid 2px red;
-        margin-left: 15px;
-        z-index: 999;
-        animation: title-new ease-in 0.2s;
 
-    }
-    .navbar-collapse{
-        z-index: 88;
-    }
-    .dropdown:hover .list-news {
-        display: block;
-    }
-
-    .item-news {
-        display: block;
-        margin: 15px 0px;
-
-    }
-
-    .title-news {
-        display: block;
-        text-align: start;
-        font-size: 16px;
-        font-weight: 350;
-
-    }
-    .title-news:hover{
-        text-decoration: underline;
-    }
-    .dropdown {
-        display: block;
-        margin: 0 35px;
-    }
-    @keyframes title-new{
-        from{
-            opacity: 0;
-            transform: scale(0);
-        }
-        to{
-            opacity: 1;
-            transform: scale(1);
-        }
-    }
-
-</style>
 {{--//test--}}
--- resources/views/frontend/includes/master.blade.php
+++ resources/views/frontend/includes/master.blade.php
@@ -30,7 +30,7 @@
 <script src="{{ asset('frontend/auth.js') }}"></script>
 <script src="{{ asset('frontend/js/notify.min.js') }}"></script>
 @yield('script')
-
+{{ HTML::script('frontend/main.js') }}
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
