40c7f05b39716c66d09e03a8e64f88293a1abe41
0979304808 <hungh5614@gmail.com>
2022-01-08 21:46:13 +0700

    a

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Core\Traits\ApiResponser;
+use App\Repositories\Categories\CategoryRepository;
+use App\Repositories\Posts\Contract\PostRepositoryInterface;
+use App\Repositories\Users\Contract\UserRepositoryInterface;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    use ApiResponser;
+
+    private $post, $category, $user;
+    public function __construct(PostRepositoryInterface $post, CategoryRepository $category, UserRepositoryInterface $user)
+    {
+        $this->post = $post;
+        $this->category = $category;
+        $this->user = $user;
+    }
+    public function list()
+    {
+        $posts = $this->post->all(['title', 'description']);
+        return $this->success($posts,'200');
+    }
+}
